554caca3f9937598ea3ad5b99744a90fe5a0f949
Will Nations <willnationsdev@gmail.com>
2017-10-25 12:48:51 -0500

    added basic plugin files

---
+++ addons/godot-data-structures/godot_data_structures_plugin.gd
@@ -0,0 +1,34 @@
+tool
+extends EditorPlugin
+
+##### CLASSES #####
+
+##### SIGNALS #####
+
+##### CONSTANTS #####
+
+##### EXPORTS #####
+
+##### MEMBERS #####
+
+##### NOTIFICATIONS #####
+
+func _enter_tree():
+	pass
+
+func _exit_tree():
+	pass
+
+##### OVERRIDES #####
+
+##### VIRTUALS #####
+
+##### PUBLIC METHODS #####
+
+##### PRIVATE METHODS #####
+
+##### SETTERS AND GETTERS #####
+
+
+
+
